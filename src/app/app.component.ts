b109ae01a581dcef82ca6273cbc363286ec8d07e
Eugen Neufeld <eneufeld@eclipsesource.com>
2017-04-07 18:55:00 +0200

    added jsonforms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import 'jsonforms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,109 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'JSONForms Angular Seed!';
+  private schema = {
+    'type': 'object',
+    'properties': {
+      'name': {
+        'type': 'string',
+        'minLength': 3
+      },
+      'personalData': {
+        'type': 'object',
+        'properties': {
+          'age': {
+            'type': 'integer'
+          },
+          'height': {
+            'type': 'number'
+          }
+        },
+        'required': ['age', 'height']
+      },
+      'vegetarian': {
+        'type': 'boolean'
+      },
+      'birthDate': {
+        'type': 'string',
+        'format': 'date'
+      },
+      'nationality': {
+        'type': 'string',
+        'enum': ['DE', 'IT', 'JP', 'US', 'RU', 'Other']
+      },
+      'occupation': {
+        'type': 'string'
+      }
+    },
+    'required': ['occupation', 'nationality']
+  };
+  private uischema = {
+    'type': 'VerticalLayout',
+    'elements': [
+      {
+        'type': 'HorizontalLayout',
+        'elements': [
+          {
+            'type': 'Control',
+            'label': {
+              'text': 'Name',
+              'show': true
+            },
+            'scope': {
+              '$ref': '#/properties/name'
+            }
+          },
+          {
+            'type': 'Control',
+            'label': {
+              'text': 'Age'
+            },
+            'scope': {
+              '$ref': '#/properties/personalData/properties/age'
+            }
+          },
+          {
+            'type': 'Control',
+            'label': 'Height',
+            'scope': {
+              '$ref': '#/properties/personalData/properties/height'
+            }
+          }
+        ]
+      },
+      {
+        'type': 'HorizontalLayout',
+        'elements': [
+          {
+            'type': 'Control',
+            'label': 'Nationality',
+            'scope': {
+              '$ref': '#/properties/nationality'
+            }
+          },
+          {
+            'type': 'Control',
+            'label': 'Occupation',
+            'scope': {
+              '$ref': '#/properties/occupation'
+            },
+            'suggestion': ['Accountant', 'Engineer', 'Freelancer',
+              'Journalism', 'Physician', 'Student', 'Teacher', 'Other']
+          },
+          {
+            'type': 'Control',
+            'label': 'Birthday',
+            'scope': {
+              '$ref': '#/properties/birthDate'
+            }
+          }
+        ]
+      }
+    ]
+  };
+  private data = {
+    name: 'John Doe',
+    birthDate: '1985-06-02'
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -15,6 +15,7 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
