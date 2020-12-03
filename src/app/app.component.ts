e5914e8eec4107fdc3f8ea3c14f3960ceb91de03
Eugen Neufeld <eneufeld@eclipsesource.com>
2020-12-03 09:41:25 +0100

    Update seed to new jsonforms root component
    This updates the seed to use the new root component.
    - Removed the store
    - move necessary parameters into app.component
    - update to jsonform 2.5.0-alpha.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import { angularMaterialRenderers } from '@jsonforms/angular-material';
+import { and, createAjv, isControl, optionIs, rankWith, schemaTypeIs, scopeEndsWith, Tester } from '@jsonforms/core';
+import { CustomAutocompleteControlRenderer } from './custom.autocomplete';
+import { DataDisplayComponent } from './data.control';
+import { LangComponent } from './lang.control';
+import uischemaAsset from '../assets/uischema.json';
+import schemaAsset from '../assets/schema.json';
+import dataAsset from './data';
+import AJV from 'ajv';
+import { parsePhoneNumber } from 'libphonenumber-js';
+
+const departmentTester: Tester = and(
+  schemaTypeIs('string'),
+  scopeEndsWith('department')
+);
 
 @Component({
   selector: 'app-root',
@@ -6,4 +21,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  renderers = [
+    ...angularMaterialRenderers,
+    { tester: rankWith(5, departmentTester), renderer: CustomAutocompleteControlRenderer },
+    {
+      renderer: DataDisplayComponent,
+      tester: rankWith(
+        6,
+        and(
+          isControl,
+          scopeEndsWith('___data')
+        )
+      )
+    },
+    {
+      renderer: LangComponent,
+      tester: rankWith(
+        6,
+        and(
+          isControl,
+          optionIs('lang', true)
+        )
+      )
+    },
+  ];
+  uischema = uischemaAsset;
+  schema = schemaAsset;
+  data = dataAsset;
+  ajv = createAjv({
+    schemaId: 'auto',
+    allErrors: true,
+    jsonPointers: true,
+    errorDataPath: 'property'
+  });
+  constructor() {
+    this.ajv.addFormat('time', '^([0-1][0-9]|2[0-3]):[0-5][0-9]$');
+    this.ajv.addFormat('tel', maybePhoneNumber => {
+      try {
+        parsePhoneNumber(maybePhoneNumber, 'DE');
+        return true;
+      } catch (_) {
+        return false;
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,63 +1,17 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { JsonFormsAngularService, JsonFormsModule } from '@jsonforms/angular';
+import { JsonFormsModule } from '@jsonforms/angular';
 import { JsonFormsAngularMaterialModule } from '@jsonforms/angular-material';
-import { Actions, setLocale, UISchemaElement } from '@jsonforms/core';
-import AJV from 'ajv';
-import JsonRefs from 'json-refs';
-import { parsePhoneNumber } from 'libphonenumber-js';
-import { forkJoin } from 'rxjs';
 import { AppComponent } from './app.component';
 import { CustomAutocompleteControlRenderer } from './custom.autocomplete';
-import data from './data';
 import { DataDisplayComponent } from './data.control';
 import { LangComponent } from './lang.control';
-import { initialState } from './store';
 
-export const loadCore = (jsonformsService: JsonFormsAngularService, http: HttpClient): () => Promise<void> => {
-  return () => {
-    const ajv = new AJV({
-      schemaId: 'auto',
-      allErrors: true,
-      jsonPointers: true,
-      errorDataPath: 'property'
-    });
-    ajv.addFormat('time', '^([0-1][0-9]|2[0-3]):[0-5][0-9]$');
-    ajv.addFormat('tel', maybePhoneNumber => {
-      try {
-        parsePhoneNumber(maybePhoneNumber, 'DE');
-        return true;
-      } catch (_) {
-        return false;
-      }
-    });
 
-    return forkJoin({
-      uischema: http.get('./assets/uischema.json'),
-      schema: http.get('./assets/schema.json')
-    }).toPromise().then(result => {
-      const { schema, uischema } = result;
-      return JsonRefs.resolveRefs(schema)
-        .then(
-          (res: any) => {
-            jsonformsService.updateCore(
-              Actions.init(
-                data,
-                res.resolved,
-                uischema as UISchemaElement,
-                ajv as any
-              )
-            );
-          }
-
-        );
-    });
-  };
-};
 @NgModule({
   declarations: [
     AppComponent,
@@ -75,23 +29,8 @@ export const loadCore = (jsonformsService: JsonFormsAngularService, http: HttpCl
     HttpClientModule
   ],
   schemas: [],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: loadCore,
-      deps: [JsonFormsAngularService, HttpClient],
-      multi: true
-    }
-  ],
   entryComponents: [CustomAutocompleteControlRenderer, LangComponent, DataDisplayComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(jsonformsService: JsonFormsAngularService) {
-    jsonformsService.init(initialState.jsonforms);
-
-    jsonformsService.updateLocale(setLocale('de-DE'));
-
-
-  }
 }
